727fc16c4298646bacd50b83d4d5459483de5fc2
Paulo Andrino <pauloandrino@yahoo.com.br>
2022-05-12 15:36:02 -0300

    Security API - Authorization Code flow allow to receive client id and secret on form

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -64,7 +64,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.checkTokenAccess("isAuthenticated()");
+        security.checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
     }
 
     @Override
