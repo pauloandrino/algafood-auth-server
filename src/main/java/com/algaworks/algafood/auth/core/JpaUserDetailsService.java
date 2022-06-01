ae13e58c7e8651d346e83bd8adb24ff725e95314
Paulo Andrino <pauloandrino@yahoo.com.br>
2022-06-01 15:21:20 -0300

    Security API - Retrieving authorities on login

--- src/main/java/com/algaworks/algafood/auth/core/JpaUserDetailsService.java
+++ src/main/java/com/algaworks/algafood/auth/core/JpaUserDetailsService.java
@@ -3,12 +3,17 @@ package com.algaworks.algafood.auth.core;
 import com.algaworks.algafood.auth.domain.Usuario;
 import com.algaworks.algafood.auth.domain.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
@@ -16,11 +21,19 @@ public class JpaUserDetailsService implements UserDetailsService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var usuario = usuarioRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email informado"));
 
-        return new AuthUser(usuario);
+        return new AuthUser(usuario, getAuthorities(usuario));
+    }
+
+    private Collection<GrantedAuthority> getAuthorities(Usuario usuario) {
+        return usuario.getGrupos().stream()
+                .flatMap(grupo -> grupo.getPermissoes().stream())
+                .map(permissao -> new SimpleGrantedAuthority(permissao.getNome().toUpperCase()))
+                .collect(Collectors.toSet());
     }
 }
--- src/main/java/com/algaworks/algafood/auth/domain/Grupo.java
+++ src/main/java/com/algaworks/algafood/auth/domain/Grupo.java
@@ -17,7 +17,7 @@ import java.util.Set;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @Entity
-public class Usuario {
+public class Grupo {
 
     @EqualsAndHashCode.Include
     @Id
@@ -27,16 +27,10 @@ public class Usuario {
     @Column(nullable = false)
     private String nome;
 
-    @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String senha;
-
     @ManyToMany
-    @JoinTable(name = "usuario_grupo",
-            joinColumns = @JoinColumn(name = "usuario_id"),
-            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-    private Set<Grupo> grupos = new HashSet<>();
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private Set<Permissao> permissoes = new HashSet<>();
 
 }
