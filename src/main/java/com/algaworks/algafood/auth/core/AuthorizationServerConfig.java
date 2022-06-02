3595d5c66997b29abc4ba1f0882011eb79810dec
Paulo Andrino <pauloandrino@yahoo.com.br>
2022-06-02 10:27:36 -0300

    Security API - Load granted authorities from OAuth2 scopes

--- src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
@@ -49,7 +49,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("algafood-web")
                 .secret(passwordEncoder.encode("web123"))
                 .authorizedGrantTypes("password", "refresh_token")
-                .scopes("write", "read")
+                .scopes("WRITE", "READ")
                 .accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
                 .refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 
@@ -57,20 +57,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("foodanalytics")
                 .secret(passwordEncoder.encode("food123"))
                 .authorizedGrantTypes("authorization_code")
-                .scopes("write", "read")
+                .scopes("WRITE", "READ")
                 .redirectUris("http://aplicacao-cliente")
 
                 .and()
                 .withClient("webadmin")
                 .authorizedGrantTypes("implicit")
-                .scopes("write", "read")
+                .scopes("WRITE", "READ")
                 .redirectUris("http://aplicacao-cliente")
 
                 .and()
                 .withClient("faturamento")
                 .secret(passwordEncoder.encode("faturamento123"))
                 .authorizedGrantTypes("client_credentials")
-                .scopes("write", "read")
+                .scopes("WRITE", "READ")
 
                 .and()
                 .withClient("checktoken")
